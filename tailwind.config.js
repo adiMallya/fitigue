7acbefc8bc525610e19a60460841c6792dcdab91
adiMallya <adimallya99@gmail.com>
2023-09-29 17:50:09 +0530

    chore: project setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
