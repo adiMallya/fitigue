757133bb7251f59b08af4093e1bc923e6ce294a4
adiMallya <adimallya99@gmail.com>
2023-10-09 01:29:38 +0530

    refactor: array type changed

--- src/utils/sortByCreatedTime.ts
+++ src/utils/sortByCreatedTime.ts
@@ -1,7 +1,10 @@
 import { ActivityData } from "src/features/activity/types";
+import { FoodData } from "src/features/food/types";
 
-export function sortByCreatedTime(activites: ActivityData[]): ActivityData[] {
-    return activites.sort((a, b) => {
+type ArrayType = ActivityData | FoodData;
+
+export function sortByCreatedTime(data: ArrayType[]): ArrayType[] {
+    return data.sort((a, b) => {
         const dateA = new Date(a?.createdAt);
         const dateB = new Date(b?.createdAt);
 
