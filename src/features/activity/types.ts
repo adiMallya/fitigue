ecf635df43ef0994468e24f4bf845836434330ac
adiMallya <adimallya99@gmail.com>
2023-10-08 19:14:24 +0530

    add: types for activity state, actions

---
+++ src/features/activity/types.ts
@@ -0,0 +1,42 @@
+//shape of activity data
+export interface ActivityData {
+    _id: string;
+    name: string;
+    duration: number;
+    calories: number;
+    [key: string]: any;
+}
+
+// shape of user state
+export interface ActivityState {
+    activities: ActivityData[];
+    loading: boolean;
+    error: string | null;
+}
+
+// action types
+export const ACTIVITY_SUCCESS = 'activity/success';
+export const ACTIVITY_PENDING = 'activity/pending';
+export const ACTIVITY_ERROR = 'activity/error';
+export const ACTIVITY_DELETE = 'activity/delete';
+
+interface ActivitySuccessAction {
+    type: typeof ACTIVITY_SUCCESS;
+    payload: ActivityData[];
+}
+
+interface ActivityDeleteAction {
+    type: typeof ACTIVITY_DELETE;
+    payload: string;
+}
+
+interface ActivityErrorAction {
+    type: typeof ACTIVITY_ERROR;
+    payload: string
+}
+
+interface ActivityPendingAction {
+    type: typeof ACTIVITY_PENDING;
+}
+
+export type ActivityActionType = ActivitySuccessAction | ActivityDeleteAction | ActivityPendingAction | ActivityErrorAction;
