9a29f1b7736cf0de545ec66c562b8dbb86569665
adiMallya <adimallya99@gmail.com>
2023-10-09 10:48:16 +0530

    add: types for foal state, actions

---
+++ src/features/goals/types.ts
@@ -0,0 +1,55 @@
+//shape of goal data
+export interface GoalData {
+    _id: string;
+    name: string;
+    description: string;
+    targetDate: string;
+    targetCalories: number;
+    status: 'In Progress' | 'Acheived' | 'Abandoned';
+    [key: string]: any;
+}
+
+// shape of goal state
+export interface GoalState {
+    goals: GoalData[];
+    loading: boolean;
+    error: string | null;
+}
+
+// action types
+export const GOAL_SUCCESS = 'goal/success';
+export const GOAL_UPDATE = 'goal/update';
+export const GOAL_PENDING = 'goal/pending';
+export const GOAL_ERROR = 'goal/error';
+export const GOAL_DELETE = 'goal/delete';
+export const CLEAR_ERROR = 'clear/error';
+
+interface GoalSuccessAction {
+    type: typeof GOAL_SUCCESS;
+    payload: GoalData[];
+}
+
+interface GoalUpdateAction {
+    type: typeof GOAL_UPDATE;
+    payload: 'In Progress' | 'Acheived' | 'Abandoned';
+}
+
+interface GoalDeleteAction {
+    type: typeof GOAL_DELETE;
+    payload: string;
+}
+
+interface GoalErrorAction {
+    type: typeof GOAL_ERROR;
+    payload: string
+}
+
+interface GoalPendingAction {
+    type: typeof GOAL_PENDING;
+}
+
+interface ClearErrorAction {
+    type: typeof CLEAR_ERROR;
+}
+
+export type GoalActionType = GoalSuccessAction | GoalUpdateAction | GoalDeleteAction | GoalPendingAction | GoalErrorAction | ClearErrorAction;
