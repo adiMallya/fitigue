247e4c7ab4ca40f12e3e2daec8394021303f418a
adiMallya <adimallya99@gmail.com>
2023-10-10 13:05:00 +0530

    add: CustomDate component

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -2,4 +2,5 @@ export { Loader } from "./Loader";
 export { ButtonLoader } from "./ButtonLoader";
 export { Navbar } from "./Navbar";
 export { CustomSelect } from "./CustomSelect";
+export { CustomDate } from "./CustomDate";
 export { SkeletonCard } from "./SkeletonCard";
